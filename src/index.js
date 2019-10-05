f5813a23a41272b2078e797660d540380cf1bea7
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-10-05 22:31:34 +0300

    init: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+  console.log(str);
+  
+  var k=0;
+  var i=0;
+
+  for (i=0; i<str.length; i++) {
+    if (str[i]==bracketsConfig[0,0]) {
+      k++;
+    } else if (str[i+1]==bracketsConfig[0,1]) {
+      k--;
+    }
+    i=i+2;
+  };
+
+  console.log('k=' + k);
+  var result;
+  if (k == 0) { result=true}
+  else if (k!==0) { result=false};
+  console.log('Result=' + result);
+  return result;
 }
