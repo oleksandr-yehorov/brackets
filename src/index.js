45d86e26bb8e0fb25191d5513dbbc8efac53a110
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-10-05 23:31:48 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
 
- const arr = str.split('');  
- if (arr.length % 2 !== 0) return false;
+  const arr = str.split('');  // split string
+  if (arr.length % 2 !== 0) return false;
 
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < bracketsConfig.length; j++) {
-            if (arr[i] === bracketsConfig[j][0] && arr[i + 1] === bracketsConfig[j][1]) {
-              arr.splice(i, 2); // delete 2 brackets : opened and closed
-              let newStr = arr.join(''); // make new string 
-              return check(newStr, bracketsConfig); // recursion function
-            }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+        if (arr[i] === bracketsConfig[j][0] && arr[i + 1] === bracketsConfig[j][1]) {
+            arr.splice(i, 2); // delete 2 brackets : opened and closed
+            let newStr = arr.join(''); // make new string 
+            return check(newStr, bracketsConfig); // recursion function
         }
     }
+  }
     
-    if (arr.length == 0) {
-      return true
-     }
-      else {
-    return false
-    };
+  return (arr.length == 0) ? true : false;
 
 }
